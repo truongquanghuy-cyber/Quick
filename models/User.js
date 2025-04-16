5d68172eb0a08f7a8ee8c4f190511a407babb5e2
huyhoang2564 <truongquanghuy1709@gmail.com>
2025-04-16 20:32:18 +0700

    first commit

---
+++ models/User.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    _id: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    imageUrl: { type: String, required: true },
+    cartItems: { type: Object, required: true },
+  },
+  { minimize: false }
+);
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
